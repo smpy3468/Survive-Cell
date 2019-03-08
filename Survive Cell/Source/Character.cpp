ee378c33414faa0992a5a6ad564e96e9642f366e
smpy3468 <smpy3468@gmail.com>
2019-03-08 17:00:18 +0800

    Character的Get和Set

--- Survive Cell/Source/Character.cpp
+++ Survive Cell/Source/Character.cpp
@@ -5,3 +5,103 @@ Character::Character()
 {
 	tag = "Character";
 }
+
+void Character::SetMaxHP(int maxHp)
+{
+	this->maxHP = maxHp;
+}
+
+void Character::SetHP(int hp)
+{
+	this->HP = hp;
+}
+
+void Character::SetOriginMoveSpeed(int originMoveSpeed)
+{
+	this->originMoveSpeed = originMoveSpeed;
+}
+
+void Character::SetMoveSpeed(int moveSpeed)
+{
+	this->moveSpeed = moveSpeed;
+}
+
+void Character::SetOriginAttack(int originAttack)
+{
+	this->originAttack = originAttack;
+}
+
+void Character::SetAttack(int attack)
+{
+	this->attack = attack;
+}
+
+void Character::SetOriginAttackSpeed(int originAttackSpeed)
+{
+	this->originAttackSpeed = originAttackSpeed;
+}
+
+void Character::SetAttackSpeed(int attackSpeed)
+{
+	this->attackSpeed = attackSpeed;
+}
+
+void Character::SetOriginAttackRange(int originAttackRange)
+{
+	this->originAttackRange = originAttackRange;
+}
+
+void Character::SetAttackRange(int attackRange)
+{
+	this->attackRange = attackRange;
+}
+
+void Character::SetIsDead(bool isDead)
+{
+	this->isDead = isDead;
+}
+
+int Character::GetMaxHP()
+{
+	return this->maxHP;
+}
+
+int Character::GetHP()
+{
+	return this->HP;
+}
+
+int Character::GetOriginMoveSpeed()
+{
+	return this->originMoveSpeed;
+}
+
+int Character::GetMoveSpeed()
+{
+	return this->moveSpeed;
+}
+
+int Character::GetOriginAttack()
+{
+	return this->originAttack;
+}
+
+int Character::GetAttack()
+{
+	return this->attack;
+}
+
+int Character::GetOriginAttackRange()
+{
+	return this->originAttackRange;
+}
+
+int Character::GetAttackRange()
+{
+	return this->attackRange;
+}
+
+bool Character::IsDead()
+{
+	return this->isDead;
+}
--- Survive Cell/Source/Character.h
+++ Survive Cell/Source/Character.h
@@ -4,11 +4,43 @@
 //角色
 //繼承GameObject
 
+#include <vector>
 #include "GameObject.h"
+#include "Buff.h"
 
 class Character :public GameObject
 {
 public:
 	Character();
+	void SetMaxHP(int maxHp);//設定最大血量
+	void SetHP(int hp);//設定目前血量
+	void SetOriginMoveSpeed(int originMoveSpeed);//設定原始移動速度
+	void SetMoveSpeed(int moveSpeed);//設定目前移動速度
+	void SetOriginAttack(int originAttack);//設定原始攻擊力
+	void SetAttack(int attack);//設定目前攻擊力
+	void SetOriginAttackSpeed(int originAttackSpeed);//設定原始攻擊速度
+	void SetAttackSpeed(int attackSpeed);//設定目前攻擊速度
+	void SetOriginAttackRange(int originAttackRange);//設定原始攻擊範圍
+	void SetAttackRange(int attackRange);//設定目前攻擊範圍
+	void SetIsDead(bool isDead);//設定是否死亡
+
+	int GetMaxHP();//取得最大血量
+	int GetHP();//取得目前血量
+	int GetOriginMoveSpeed();//取得原始移動速度
+	int GetMoveSpeed();//取得目前移動速度
+	int GetOriginAttack();//取得原始攻擊力
+	int GetAttack();//取得目前攻擊力
+	int GetOriginAttackRange();//取得原始攻擊範圍
+	int GetAttackRange();//取得攻擊範圍
+	bool IsDead();//確認是否死亡
+
 private:
+	int maxHP, HP;//最大血量、目前血量
+	int originMoveSpeed, moveSpeed;//原始移動速度、目前移動速度
+	int originAttack, attack;//原始攻擊力、目前攻擊力
+	int	originAttackSpeed, attackSpeed;//原始攻擊速度、目前攻擊速度
+	int originAttackRange, attackRange;//原始攻擊範圍、目前攻擊範圍
+
+	vector<Buff> buff;//套用中的BUFF
+	bool isDead;//是否死亡
 };
