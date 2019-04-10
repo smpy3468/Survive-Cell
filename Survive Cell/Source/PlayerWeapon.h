811a5dd6c1bb7f5886188a252d65d1f065943e9a
Moster_YJ <s88037zz@gmail.com>
2019-04-10 20:49:22 +0800

    Player* player 宣告在ＰlayerEquipment

--- Survive Cell/Source/PlayerWeapon.h
+++ Survive Cell/Source/PlayerWeapon.h
@@ -1,5 +1,6 @@
 #pragma once
 #include "PlayerEquipment.h"
+#include "Player.h"
 
 class PlayerWeapon :public PlayerEquipment
 {
@@ -10,7 +11,6 @@ protected:
 	bool haveEffect = false;         //使否 有特效;
 	int playerCurrentAni = 0, playerAniNumber = 0; //用來判斷玩家攻擊動畫進行到哪個階段，進而顯示Effect
 
-	Player* player = GameSystem::GetGameObjectWithTag<Player>("Player");
 
 	const int ANI_ATTACK_LEFT = 3, ANI_ATTACK_RIGHT = 4;						//攻擊動編碼
 };
