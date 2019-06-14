d06046feaa71a1dd6aef3beb118ca6470e3e0850
Moster_YJ <s88037zz@gmail.com>
2019-06-14 08:34:22 +0800

    end

--- Survive Cell/Source/gamelib.h
+++ Survive Cell/Source/gamelib.h
@@ -73,7 +73,7 @@
 
 #define SIZE_X				 1366//640		// 設定遊戲畫面的解析度為640x480
 #define SIZE_Y				 768//480		// 註：若不使用標準的解析度，則不能切換到全螢幕
-#define OPEN_AS_FULLSCREEN	 false		// 是否以全螢幕方式開啟遊戲
+#define OPEN_AS_FULLSCREEN	 true		// 是否以全螢幕方式開啟遊戲
 #define SHOW_LOAD_PROGRESS   true		// 是否顯示loading(OnInit)的進度
 #define DEFAULT_BG_COLOR	 RGB(255,255,255)	// 遊戲畫面預設的背景顏色(黑色)
 #define GAME_CYCLE_TIME		 33		    // 每33ms跑一次Move及Show(每秒30次)
